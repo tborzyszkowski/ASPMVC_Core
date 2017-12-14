542bed88ed719c182e5e2a6248ccd651b03d9ef5
tborzyszkowski <t.borzyszkowski@gmail.com>
2017-12-14 23:39:18 +0100

    MVC Movie Sample

---
+++ 01Wstep/MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie : IValidatableObject {
+        private const int _expensive = 1000;
+        private const int _oldYear = 1960;
+
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [OlderMovie(1960)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Range(0, 9999.99)]
+        public decimal Price { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) {
+            if (Price > _expensive && ReleaseDate.Year <= _oldYear) {
+                yield return new ValidationResult(
+                    $"Old movies are not so expensive: {_expensive}.",
+                    new[] { "ReleaseDate" });
+            }
+        }
+    }
+}
---
+++ 01Wstep/MvcMovie/MvcMovie/Models/OlderMovieAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models {
+    public class OlderMovieAttribute : ValidationAttribute {
+        private int _year;
+
+        public OlderMovieAttribute(int Year) {
+            _year = Year;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext) {
+            Movie movie = (Movie)validationContext.ObjectInstance;
+
+            if (movie.ReleaseDate.Year > _year) {
+                return new ValidationResult(GetErrorMessage());
+            }
+
+            return ValidationResult.Success;
+        }
+
+        private string GetErrorMessage() {
+            return $"Movies must be older than {_year}.";
+        }
+    }
+}
