3a0f593409120bd17b17578bbe53df6097d44570
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-01-05 19:39:12 +0100

    Dodany projekt BulletinBoard - github.com/kkulewski

---
+++ ProjektyStudentow/2017_KulewskiKrzysztof/BulletinBoard/Controllers/RoleController.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BulletinBoard.Data;
+using BulletinBoard.Helpers;
+using BulletinBoard.Models;
+using BulletinBoard.Models.RoleViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulletinBoard.Controllers
+{
+    [Authorize(Roles = RoleHelper.Administrator)]
+    public class RoleController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RoleController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Role
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _context.Roles.ToListAsync();
+            var users = await _context.ApplicationUsers.ToListAsync();
+
+            var viewModel = users.Select(user =>
+            {
+                var userRole = _userManager.GetRolesAsync(user).Result.FirstOrDefault();
+                var userRoleId = _context.Roles.FirstOrDefault(role => role.NormalizedName == userRole).Id;
+                var vm = new RoleViewModel
+                {
+                    ApplicationUser = user,
+                    ApplicationUserId = user.Id,
+                    RoleId = userRoleId,
+                    Roles = roles,
+                    Disabled = string.Equals(userRole, RoleHelper.Administrator)
+                };
+                return vm;
+            });
+
+            return View(viewModel);
+        }
+
+        // POST: Role/ChangeRole
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangeRole(string applicationUserId, string roleId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            var user = _context.ApplicationUsers.First(c => c.Id == applicationUserId);
+            var newRole = _context.Roles.First(c => c.Id == roleId);
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+            await _userManager.RemoveFromRolesAsync(user, userRoles);
+            await _userManager.AddToRoleAsync(user, newRole.Name);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ ProjektyStudentow/2017_KulewskiKrzysztof/BulletinBoard/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BulletinBoard.Models;
+
+namespace BulletinBoard.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        // declare each database entity here to access it with dbContext
+        public virtual DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public virtual DbSet<JobCategory> JobCategories { get; set; }
+        public virtual DbSet<JobType> JobTypes { get; set; }
+        public virtual DbSet<JobOffer> JobOffers { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+    }
+}
