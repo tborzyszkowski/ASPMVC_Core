09eb97a34e89007c9f4757813ad6cd715f07aaf7
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2020-01-05 20:11:57 +0100

    wykład 2020 PJ

--- 02Widoki/Person/Person/Migrations/20180111211558_Address.cs
+++ 02Widoki/Person/Person/Migrations/20180111211558_Address.cs
@@ -1,64 +1,58 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
-using System;
-using System.Collections.Generic;
 
-namespace Person.Migrations
-{
-    public partial class Address : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<int>(
-                name: "AddressID",
-                table: "Human",
-                nullable: true);
+namespace Person.Migrations {
+	public partial class Address : Migration {
+		protected override void Up(MigrationBuilder migrationBuilder) {
+			migrationBuilder.AddColumn<int>(
+				name: "AddressID",
+				table: "Human",
+				nullable: true);
 
-            migrationBuilder.CreateTable(
-                name: "Address",
-                columns: table => new
-                {
-                    ID = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    Code = table.Column<string>(nullable: true),
-                    Street = table.Column<string>(nullable: true),
-                    Town = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Address", x => x.ID);
-                });
+			migrationBuilder.CreateTable(
+				name: "Address",
+				columns: table => new
+				{
+					ID = table.Column<int>(nullable: false)
+						.Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+					Code = table.Column<string>(nullable: true),
+					Street = table.Column<string>(nullable: true),
+					Town = table.Column<string>(nullable: true)
+				},
+				constraints: table =>
+				{
+					table.PrimaryKey("PK_Address", x => x.ID);
+				});
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Human_AddressID",
-                table: "Human",
-                column: "AddressID");
+			migrationBuilder.CreateIndex(
+				name: "IX_Human_AddressID",
+				table: "Human",
+				column: "AddressID");
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_Human_Address_AddressID",
-                table: "Human",
-                column: "AddressID",
-                principalTable: "Address",
-                principalColumn: "ID",
-                onDelete: ReferentialAction.Restrict);
-        }
+			migrationBuilder.AddForeignKey(
+				name: "FK_Human_Address_AddressID",
+				table: "Human",
+				column: "AddressID",
+				principalTable: "Address",
+				principalColumn: "ID",
+				onDelete: ReferentialAction.Restrict);
+		}
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropForeignKey(
-                name: "FK_Human_Address_AddressID",
-                table: "Human");
+		protected override void Down(MigrationBuilder migrationBuilder) {
+			migrationBuilder.DropForeignKey(
+				name: "FK_Human_Address_AddressID",
+				table: "Human");
 
-            migrationBuilder.DropTable(
-                name: "Address");
+			migrationBuilder.DropTable(
+				name: "Address");
 
-            migrationBuilder.DropIndex(
-                name: "IX_Human_AddressID",
-                table: "Human");
+			migrationBuilder.DropIndex(
+				name: "IX_Human_AddressID",
+				table: "Human");
 
-            migrationBuilder.DropColumn(
-                name: "AddressID",
-                table: "Human");
-        }
-    }
+			migrationBuilder.DropColumn(
+				name: "AddressID",
+				table: "Human");
+		}
+	}
 }
