c8d78d8896b06351901d5bb84426f736dd649bd3
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2020-01-06 18:43:01 +0100

    DB connection

--- 01Modele/MvcMovie/MvcMovie/Controllers/MoviesController.cs
+++ 01Modele/MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -7,185 +7,173 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MvcMovie.Models;
 
-namespace MvcMovie.Controllers
-{
-    public class MoviesController : Controller
-    {
-        private readonly MvcMovieContext _context;
+namespace MvcMovie.Controllers {
+	public class MoviesController : Controller {
+		private readonly MvcMovieContext _context;
 
-        public MoviesController(MvcMovieContext context)
-        {
-            _context = context;
-        }
+		public MoviesController(MvcMovieContext context) {
+			_context = context;
+		}
 
-        // GET: Movies
-        //public async Task<IActionResult> Index()
-        //{
-        //    return View(await _context.Movie.ToListAsync());
-        //}
+		// GET: Movies
+		//public async Task<IActionResult> Index()
+		//{
+		//    return View(await _context.Movie.ToListAsync());
+		//}
 
-        public async Task<IActionResult> Index(string searchString) {
-            var movies = from m in _context.Movie
-                         select m;
+		public async Task<IActionResult> Index(string searchString) {
+			var movies = from m in _context.Movie
+						 select m;
 
-            if (!String.IsNullOrEmpty(searchString)) {
-                movies = movies.Where(s => s.Title.Contains(searchString));
-            }
+			if (!String.IsNullOrEmpty(searchString))
+			{
+				movies = movies.Where(s => s.Title.Contains(searchString));
+			}
 
-            return View(await movies.ToListAsync());
-        }
+			return View(await movies.ToListAsync());
+		}
 
-        [HttpPost]
-        public string Index(string searchString, bool notUsed) {
-            return "From [HttpPost]Index: filter on " + searchString;
-        }
+		[HttpPost]
+		public string Index(string searchString, bool notUsed) {
+			return "From [HttpPost]Index: filter on " + searchString;
+		}
 
-        // GET: Movies/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Movies/Details/5
+		public async Task<IActionResult> Details(int? id) {
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var movie = await _context.Movie
-                .SingleOrDefaultAsync(m => m.ID == id);
-            if (movie == null)
-            {
-                return NotFound();
-            }
+			var movie = await _context.Movie
+				.SingleOrDefaultAsync(m => m.ID == id);
+			if (movie == null)
+			{
+				return NotFound();
+			}
 
-            return View(movie);
-        }
+			return View(movie);
+		}
 
-        // GET: Movies/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+		// GET: Movies/Create
+		public IActionResult Create() {
+			return View();
+		}
 
-        // POST: Movies/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(movie);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(movie);
-        }
+		// POST: Movies/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie) {
+			if (ModelState.IsValid)
+			{
+				_context.Add(movie);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(movie);
+		}
 
-        // GET: Movies/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Movies/Edit/5
+		public async Task<IActionResult> Edit(int? id) {
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var movie = await _context.Movie.SingleOrDefaultAsync(m => m.ID == id);
-            if (movie == null)
-            {
-                return NotFound();
-            }
-            return View(movie);
-        }
+			var movie = await _context.Movie.SingleOrDefaultAsync(m => m.ID == id);
+			if (movie == null)
+			{
+				return NotFound();
+			}
+			return View(movie);
+		}
 
-        // POST: Movies/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
-        {
-            if (id != movie.ID)
-            {
-                return NotFound();
-            }
+		// POST: Movies/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie) {
+			if (id != movie.ID)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(movie);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MovieExists(movie.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(movie);
-        }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(movie);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!MovieExists(movie.ID))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(movie);
+		}
 
-        // GET: Movies/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Movies/Delete/5
+		public async Task<IActionResult> Delete(int? id) {
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var movie = await _context.Movie
-                .SingleOrDefaultAsync(m => m.ID == id);
-            if (movie == null)
-            {
-                return NotFound();
-            }
+			var movie = await _context.Movie
+				.SingleOrDefaultAsync(m => m.ID == id);
+			if (movie == null)
+			{
+				return NotFound();
+			}
 
-            return View(movie);
-        }
+			return View(movie);
+		}
 
-        // POST: Movies/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var movie = await _context.Movie.SingleOrDefaultAsync(m => m.ID == id);
-            _context.Movie.Remove(movie);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+		// POST: Movies/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id) {
+			var movie = await _context.Movie.SingleOrDefaultAsync(m => m.ID == id);
+			_context.Movie.Remove(movie);
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
 
-        private bool MovieExists(int id)
-        {
-            return _context.Movie.Any(e => e.ID == id);
-        }
+		private bool MovieExists(int id) {
+			return _context.Movie.Any(e => e.ID == id);
+		}
 
-        [AcceptVerbs("Get", "Post")]
-        public async Task<IActionResult> VerifyRating(string rating)
-        {
-            var movie = await _context.Movie
-                .FirstOrDefaultAsync(m => m.Rating.Equals(rating));
-            if (movie == null)
-                return Json(true);
-            else
-                return Json($"Rating {rating} is already in use.");
-        }
+		[AcceptVerbs("Get", "Post")]
+		public async Task<IActionResult> VerifyRating(string rating) {
+			var movie = await _context.Movie
+				.FirstOrDefaultAsync(m => m.Rating.Equals(rating));
+			if (movie == null)
+				return Json(true);
+			else
+				return Json($"Rating {rating} is already in use.");
+		}
 
-        [AcceptVerbs("Get", "Post")]
-        public async Task<IActionResult> VerifyTitle(string title, DateTime releaseDate)
-        {
-            var movie = await _context.Movie
-                .FirstOrDefaultAsync(m => m.Title.Equals(title) && 
-                                            m.ReleaseDate.Year == releaseDate.Year);
-            if (movie == null)
-                return Json(true);
-            else
-                return Json($"Movie {title} from year {releaseDate.Year} already exists.");
-        }
-    }
+		[AcceptVerbs("Get", "Post")]
+		public async Task<IActionResult> VerifyTitle(string title, DateTime releaseDate) {
+			var movie = await _context.Movie
+				.FirstOrDefaultAsync(m => m.Title.Equals(title) &&
+											m.ReleaseDate.Year == releaseDate.Year);
+			if (movie == null)
+				return Json(true);
+			else
+				return Json($"Movie {title} from year {releaseDate.Year} already exists.");
+		}
+	}
 }
--- 02Widoki/Person/Person/Startup.cs
+++ 02Widoki/Person/Person/Startup.cs
@@ -9,47 +9,54 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Person.Models;
 
-namespace Person
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+namespace Person {
+	public class Startup {
+		public Startup(IConfiguration configuration) {
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services) {
+			services.AddMvc();
 
-            services.AddDbContext<PersonContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("PersonContext")));
-        }
+			services.AddDbContext<PersonContext>(options =>
+					options.UseSqlServer(Configuration.GetConnectionString("PersonContext")));
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+			UpdateDatabase(app);
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseBrowserLink();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
 
-            app.UseStaticFiles();
+			app.UseStaticFiles();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-    }
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+		private static void UpdateDatabase(IApplicationBuilder app) {
+			using (var serviceScope = app.ApplicationServices
+				.GetRequiredService<IServiceScopeFactory>()
+				.CreateScope())
+			{
+				using (var context = serviceScope.ServiceProvider.GetService<PersonContext>())
+				{
+					context.Database.Migrate();
+				}
+			}
+		}
+	}
 }
